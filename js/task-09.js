f2c43b909986243b7b3ef82d73e07d1525103a2c
Dmytro Krasniuk <d.krasnyuk@gmail.com>
2023-02-04 18:37:08 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const bodyEl = document.querySelector("body");
+
+const changeColorEl = document.querySelector(".change-color");
+
+const colorEl = document.querySelector(".color");
+
+changeColorEl.addEventListener("click", changeBgColorRandom);
+
+function changeBgColorRandom() {
+  const color = `${getRandomHexColor()}`;
+  bodyEl.style.backgroundColor = color;
+  colorEl.textContent = color;
 }

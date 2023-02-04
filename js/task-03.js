c6acac7ab40d0f84794f7c792ec3b8c70bc44dbe
Dmytro Krasniuk <d.krasnyuk@gmail.com>
2023-02-04 18:36:42 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,25 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+
+const galleryListEl = document.querySelector("ul");
+const element = images
+  .map(function ({ url, alt }) {
+    return `<li><img src = '${url}' alt = '${alt}' width="320"></li>`;
+  })
+  .join("");
+galleryListEl.insertAdjacentHTML("afterbegin", element);
+
+document.body.style.margin = '30px';
+
+
+galleryListEl.style.cssText = `display: flex;
+  background: radial-gradient(circle, rgba(63,68,4,0.7581626400560224) 0%, rgba(8,48,3,1) 36%, rgba(85,1,1,1) 68%, rgba(0,0,0,1) 100%);  align-items: center;
+  box-shadow: 0px 200px 300px -15px rgba(0,0,0,0.69);
+  justify-content: center;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  `;
